c8e2e94eb8db7c81f40e9306902720fa3ab82118
Calyn Greene <calyngreene@rocketmortgage.com>
2021-10-05 10:55:13 -0400

    Initial Commit For GC Day 1 Exercises

---
+++ DevBuildDay1Exercises/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace DevBuildDay1Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*Prompt the user to enter a string. After the user enters a string, output the same string back to the console.
+
+            string input;
+
+            Console.Write("Enter a word: ");
+            input = Console.ReadLine();
+
+            Console.WriteLine(input);*/
+
+            /* Prompt the user to enter a number.After the user enters a number, add 1 to the number and output it back to the console.
+
+            int num;
+
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(num); */
+
+            /*Prompt the user to enter a number. After the user enters a number, add .5 to the number and output it back to the console.
+
+            double num;
+
+            Console.Write("Enter a number: ");
+            num = double.Parse(Console.ReadLine());
+            num += .5;
+
+            Console.WriteLine(num); */
+
+            /* Prompt the user to enter two numbers. After the user enters the numbers, add them together and output the sum back to the console.
+
+            double num1, num2, sum;
+
+            Console.Write("Enter a number: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            num2 = double.Parse(Console.ReadLine());
+            sum = num1 + num2;
+            Console.WriteLine(sum); */
+
+            /* Prompt the user to enter two numbers. After the user enters the numbers, multiply them and output the product back to the console.
+
+            double num1, num2, sum;
+
+            Console.Write("Enter a number: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            num2 = double.Parse(Console.ReadLine());
+            sum = num1 * num2;
+            Console.WriteLine($"The product is {sum}"); */
+
+
+
+
+
+
+
+        }
+    }
+}
